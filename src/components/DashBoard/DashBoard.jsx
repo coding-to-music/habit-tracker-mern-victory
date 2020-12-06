2a5d9b138ab071a84fc8740bb587f4e1b43688f1
Madiha Kamran <madihakamran@Madihas-MacBook-Pro.local>
2020-12-05 23:49:28 -0500

    on mongo atlas now

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const DashBoard = (props) => (
   <div className="dash-board">
-    <h1>DashBoard HERE......!</h1>
+    <h1>DashBoard HEREEE......!</h1>
     <Link to="/habit" className="NavBar-link">
       Habit
     </Link>
