08980b438931513acec54d91d0512996e6a9736e
Madiha Kamran <madihakamran@Madihas-MacBook-Pro.local>
2020-12-06 02:38:53 -0500

    added crud on habit server side

--- routes/api/habits.js
+++ routes/api/habits.js
@@ -3,7 +3,43 @@ let Habit = require("../../models/habit");
 
 router.route("/").get((req, res) => {
   Habit.find()
-    .then((habits) => res.join(habits))
+    .then((habits) => res.json(habits))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+router.route("/add").post((req, res) => {
+  const name = req.body.name;
+  const description = req.body.description;
+  console.log(req.body);
+  const newHabit = new Habit({
+    name,
+    description,
+  });
+  newHabit
+    .save()
+    .then(() => res.json("habit added"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+router.route("/:id").get((req, res) => {
+  Habit.findById(req.params.id)
+    .then((habit) => res.json(habit))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+router.route("/:id").delete((req, res) => {
+  Habit.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Habit Deleted"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Habit.findById(req.params.id)
+    .then((habit) => {
+      habit.name = req.body.name;
+      habit.description = req.body.description;
+      habit
+        .save()
+        .then(() => res.json("habit updated"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
